15a71e3990252f19287a3f3feff03127fdf0c7e4
Shubham Kumar <kshubham5642@gmail.com>
2025-04-12 03:16:46 +0530

    Update Firebase.jsx

--- lec1/project3/src/context/Firebase.jsx
+++ lec1/project3/src/context/Firebase.jsx
@@ -5,13 +5,7 @@ import {getDatabase, set, ref, get, child, onValue} from "firebase/database"
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDbv0dm3w0L-sDOtQZCol1sxuLwfUBgk6Y",
-    authDomain: "app-7317d.firebaseapp.com",
-    databaseURL: "https://app-7317d-default-rtdb.firebaseio.com",
-    projectId: "app-7317d",
-    storageBucket: "app-7317d.firebasestorage.app",
-    messagingSenderId: "690495122590",
-    appId: "1:690495122590:web:b76e16ac93646d491edf8f",
+    // your google consol keys
   };
   
 
